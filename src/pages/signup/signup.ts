93b45b872613c74de874794b6572d79ccbb7c561
Joerg Schmitz <schmitz873@gmail.com>
2017-08-29 23:43:00 +0200

    Signup and Signin added (no functionality yet)

---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+  onSignup(form: NgForm){
+    console.log(form.value);
+  }
+}
