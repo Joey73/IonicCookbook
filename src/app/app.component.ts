7c7d845a13d14eec8fb5bd85bc0452a51d1cac02
Joerg Schmitz <schmitz873@gmail.com>
2017-09-05 00:22:34 +0200

    Added Authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { TabsPage } from "../pages/tabs/tabs";
 import { SigninPage } from "../pages/signin/signin";
@@ -20,6 +21,10 @@ export class MyApp {
     statusBar: StatusBar,
     splashScreen: SplashScreen,
     private menuCtrl: MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBvi8U71CozvR9ATNUiX8t5EnkYaTawH1M",
+      authDomain: "ionic2-recipebook-e5e0e.firebaseapp.com"
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
